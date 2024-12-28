776301db4dc6ea91c16650640bca51bbd9c4f5f2
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-28 15:26:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("#todo-form")
+const taskTitleInput = document.querySelector("#task-title-input")
+const todoListUl = document.querySelector("#todo-list")
+
+let tasks = []
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()//evita o carregamento padrão de recarregar a página 
+
+    const taskTitle = taskTitleInput.value
+
+    if(taskTitle.length < 3 ){
+        alert("Sua tarefa precisa ter pelo menos 3 caracteres")
+        return;
+    }
+    //Adicionando a nova tarefa no array de tasks
+    tasks.push(taskTitle)
+
+    //adicionando a nova tarefa no HTML
+} )
+
